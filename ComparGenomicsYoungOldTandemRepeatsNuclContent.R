c6465d605645c7da7a76c8d015604983f443a638
konstantinpopadin <konstantinpopadin@gmail.com>
2018-04-10 14:49:55 +0200

    Create ComparGenomicsYoungOldTandemRepeatsNuclContent.R
    It seems that young TR are a little bit GC richer than the old ones.
    It would be nice to compare the age (percent match) of each TR and it's AT content within each genome with several TRs.
    Also it would be nice to see how TR age correlates with the length of motive and number of repetitions of the motive.

---
+++ ComparGenomicsYoungOldTandemRepeatsNuclContent.R
@@ -0,0 +1,27 @@
+rm(list=ls(all=TRUE))
+user = 'Kostya'
+if (user == 'Kostya')
+{
+YoungTr = read.table('/hdd/SCIENCE_PROJECTS_BODY/MITOCHONDRIA/COMPARATIVE_GENOMICS/2_DERIVED/YoungTandemRepeats.txt', header = TRUE, sep = '\t')
+OldTr = read.table('/hdd/SCIENCE_PROJECTS_BODY/MITOCHONDRIA/COMPARATIVE_GENOMICS/2_DERIVED/OldTandemRepeats.txt', header = TRUE, sep = '\t')
+}
+
+YoungAgg = aggregate(YoungTr$ATCont, by = list(YoungTr$Species), FUN = mean)
+names(YoungAgg) = c('Species','YoungTrMeanAt')
+
+OldAgg = aggregate(OldTr$ATCont, by = list(OldTr$Species), FUN = mean)
+names(OldAgg) = c('Species','OldTrMeanAt')
+
+par(mfrow=c(2,2))
+Tr = merge(YoungAgg,OldAgg, by = 'Species')
+nrow(Tr) # 566
+hist(Tr$YoungTrMeanAt, breaks = 50); 
+hist(Tr$OldTrMeanAt, breaks = 50); 
+
+Tr$YoungAtMinusOldAt = Tr$YoungTrMeanAt - Tr$OldTrMeanAt
+summary(Tr$YoungAtMinusOldAt)
+hist(Tr$YoungAtMinusOldAt, breaks = 100)
+wilcox.test(Tr$YoungAtMinusOldAt, mu = 0)
+wilcox.test(Tr$YoungTrMeanAt,Tr$OldTrMeanAt, paired = TRUE)
+t.test(Tr$YoungTrMeanAt,Tr$OldTrMeanAt, paired = TRUE)
+
